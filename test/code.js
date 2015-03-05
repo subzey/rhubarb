729fe917c1a33019821b7c774213b4dd832c5fe4
Anton Khlynovskiy <subzey@gmail.com>
2015-03-05 17:50:03 +0300

    if statement

--- test/code.js
+++ test/code.js
@@ -1,16 +1,3 @@
-// var z = 14;
-// var u = function local (a){
-// 	console.log(a);
-// 	var z = 0;
-
-// 	function y (){
-// 		z = 22;
-// 		leak = 44;
-// 	}
-// };
-
-if ('2' + obj.method(42)){
-	console.log(foo);
-} else {
-	console.log(method);
+for (var j=0; j--; )if (!!('2' + obj.method(42))){
+	console.log(1);
 }
