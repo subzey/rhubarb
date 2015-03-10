f1182811ba68492a23c5726328421f9b2a74cd13
Anton Khlynovskiy <subzey@gmail.com>
2015-03-10 18:32:26 +0300

    Breaking: only assume partial objects that came from user

--- test/code.js
+++ test/code.js
@@ -1,1 +1,21 @@
-obj.method({'foo': 42}['foo']);
+if (Application.Features.isFeatureEnabled('foo')){
+	console.log('okay');
+} else {
+	console.log('fail');
+}
+
+if (Application.Features.isFeatureEnabled(['foo', 'zoo'][(__environment === 'prod') ? 1 : 0])){
+	console.log('fail');
+} else {
+	console.log('okay');
+}
+
+if (Application.Features.noSuchMethod(__environment)){
+	console.log('???');
+}
+
+if (42){
+	console.log('okay');
+} else {
+	console.log('okay');
+}
--- test/itworks.js
+++ test/itworks.js
@@ -1,13 +1,21 @@
-var result = require('../lib/rhubarb.node.js').inline(
-	require('fs').readFileSync('code.js', 'utf-8'), {
-		globalFoo: 33,
-		obj: {
-			foo: 42,
-			method: function(v){
-				return '<' + v + '>';
+var state = {
+	'Application': {
+		'Features': {
+			'isFeatureEnabled': function(featureName){
+				console.log('Quering ' + featureName);
+				return {
+					'foo': true,
+					'bar': true,
+					'baz': true
+				}[featureName];
 			}
-		}
+		},
 	},
+	'__environment': 'prod'
+};
+
+var result = require('../lib/rhubarb.node.js').inline(
+	require('fs').readFileSync('code.js', 'utf-8'), state,
 	{
 		scope: ''
 	}
